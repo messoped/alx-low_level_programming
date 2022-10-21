3b003ae174e670dcf6aaca890b9e8913877254d3
Your Name <iqweku3@gmail.com>
2022-10-21 15:07:07 -0700

    function that checks for uppercase character

--- 0x04-more_functions_nested_loops/_putchar.c
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -3,7 +3,7 @@
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
- * Return 1 if true ,0 if false
+ * Return 1 if true, 0 if false
  * On error, -1 return
  */
 int _putchar(char c)
