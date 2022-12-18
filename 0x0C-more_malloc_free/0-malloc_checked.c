e12b2fe5becbd66bf0bfd426e7aa2bd5600b55de
Your Name <iqweku3@gmail.com>
2022-12-18 06:55:15 -0800

    function that allocates memory using malloc.

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -4,13 +4,12 @@
  * malloc_checked - allocates memory using malloc
  * @b: amount of bytes
  * Return: pointer to our new allocated memory
- * exit with 98 if malloc fails
  */
 
 void *malloc_checked(unsigned int b)
 {
 	void *new_mem;
-	
+
 	new_mem = malloc(b);
 	if (new_mem == NULL)
 		exit(98);
